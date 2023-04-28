3de7d64f9382bf33c6f241c93853d8c46f994e01
Ahmed Farid <ahmed.farid.592002@gmail.com>
2023-04-28 08:41:32 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.inputBox = New System.Windows.Forms.RichTextBox()
+        Me.outputBox = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'inputBox
+        '
+        Me.inputBox.AcceptsTab = True
+        Me.inputBox.Location = New System.Drawing.Point(8, 8)
+        Me.inputBox.Name = "inputBox"
+        Me.inputBox.Size = New System.Drawing.Size(100, 96)
+        Me.inputBox.TabIndex = 1
+        Me.inputBox.Text = ""
+        Me.inputBox.WordWrap = False
+        '
+        'outputBox
+        '
+        Me.outputBox.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.outputBox.Location = New System.Drawing.Point(0, 354)
+        Me.outputBox.Name = "outputBox"
+        Me.outputBox.ReadOnly = True
+        Me.outputBox.Size = New System.Drawing.Size(800, 96)
+        Me.outputBox.TabIndex = 3
+        Me.outputBox.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.outputBox)
+        Me.Controls.Add(Me.inputBox)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Accessible Console"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents inputBox As RichTextBox
+    Friend WithEvents outputBox As RichTextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+	Dim con As New Process()
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		con.StartInfo = New ProcessStartInfo
+		con.StartInfo.FileName = "python.exe"
+		con.StartInfo.WorkingDirectory = "C:"
+		con.StartInfo.Arguments = "-i"
+		con.StartInfo.UseShellExecute = False
+		con.StartInfo.RedirectStandardInput = True
+		con.StartInfo.RedirectStandardOutput = True
+		con.StartInfo.RedirectStandardError = True
+		con.StartInfo.CreateNoWindow = True
+		AddHandler con.OutputDataReceived, AddressOf Process_OutputDataReceived
+		AddHandler con.ErrorDataReceived, AddressOf Process_OutputDataReceived
+		con.Start()
+		con.BeginOutputReadLine()
+		con.BeginErrorReadLine()
+		con.StandardInput.AutoFlush = True
+	End Sub
+
+	Private Sub Process_OutputDataReceived(sender As Object, e As DataReceivedEventArgs)
+		If e.Data IsNot Nothing Then
+			outputBox.AppendText(e.Data & Environment.NewLine)
+		End If
+	End Sub
+
+	Private Sub inputBox_KeyDown(sender As Object, e As KeyEventArgs) Handles inputBox.KeyDown
+		If e.KeyCode = Keys.F6 Then
+			outputBox.Focus()
+		ElseIf e.KeyCode = Keys.Enter Then
+			Dim lines() As String = inputBox.Lines
+			Dim index As Integer = inputBox.GetLineFromCharIndex(inputBox.SelectionStart)
+			Dim Input As String = inputBox.Lines(index)
+			con.StandardInput.WriteLine(Input)
+			If Input <> "" Then
+				outputBox.AppendText(Input & Environment.NewLine)
+				If index <> lines.Length - 1 Then
+					e.SuppressKeyPress = True
+				End If
+			Else
+				e.SuppressKeyPress = True
+			End If
+		End If
+	End Sub
+
+	Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+		con.Kill()
+		con.Dispose()
+	End Sub
+
+	Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+		If e.KeyCode = Keys.Escape Then
+			Me.Close()
+		ElseIf e.Control AndAlso e.KeyCode = Keys.S Then
+			Dim filePath As String
+			Dim choice As New SaveFileDialog
+			If choice.ShowDialog = DialogResult.OK Then
+				filePath = choice.FileName
+				Dim content As String = outputBox.Text
+				System.IO.File.WriteAllText(filePath, content)
+			End If
+		ElseIf e.Control And e.KeyCode = Keys.L Then
+			outputBox.Clear()
+			inputBox.Clear()
+		End If
+	End Sub
+End Class
